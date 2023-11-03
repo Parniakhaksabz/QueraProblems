14511dd8858ffd7649950bf860ba8fce4094d163
Parnia  |  پرنیا خاکسبز <149834068+Parniakhaksabz@users.noreply.github.com>
2023-11-04 03:19:34 +0330

    Add files via upload

---
+++ Solutions/3537/c++/program.cpp
@@ -0,0 +1,16 @@
+// Copy from Quera
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "W";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "o";
+    }
+    cout << "w!";
+    return 0;
+}
