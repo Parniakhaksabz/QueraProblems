ef97271277ec0c1a4708c4596e2440ebbeb0784b
Parnia  |  پرنیا خاکسبز <149834068+Parniakhaksabz@users.noreply.github.com>
2023-11-04 03:31:27 +0330

    Add files via upload

---
+++ Solutions/148099/csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Quera {
+    public static class Program {
+        public static void Main() {
+            var _ = Console.ReadLine(); //Ignore
+            var numbers = GetInputs<int>(' ');
+
+            var uniqueNumbers = numbers.GroupBy(number => number)
+                .Where(group => group.Count() == 1)
+                .Select(group => group.Key)
+                .ToList();
+
+            var xor = uniqueNumbers.Aggregate(0, (result, nextNumber) => nextNumber ^ result);
+            Console.WriteLine(xor);
+        }
+
+        private static List<T> GetInputs<T>(char separator) =>
+            Console.ReadLine()
+                ?.Trim()
+                .Split(separator)
+                .Select(item => (T) Convert.ChangeType(item, typeof(T))).ToList();
+    }
+}
